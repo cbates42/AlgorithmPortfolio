02851ae03ae24d16a7b5474d826b0dfe96fd3f7e
Cameron <64233266+zzmaxus@users.noreply.github.com>
2023-09-24 22:48:14 -0500

    added bigo example

---
+++ BigOExamples/BigOExamples/Example.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigOExamples
+{
+    internal class Example
+    {
+        private List<string> i = new List<string>();
+        void ConstantTime()
+        {
+
+            //No matter what is input, this method will always only look for the first item
+            Console.WriteLine(i[0]);
+        }
+
+        void LinearTime()
+        {
+            foreach(string item in i)
+            {
+                //Method will run once for each item in the list
+                Console.WriteLine(item);
+            }
+        }
+
+        void QuadraticTime()
+        {
+            foreach(string item in i)
+            {
+                foreach(string item2 in i)
+                {
+
+                    //Because of the nested loop, method will run an amount of times for each time the 
+                    //outer loop ran
+                    Console.WriteLine(item, item2);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AlgorithmPortfolio
- 
+
+## Big O:
+
+Big-O Notation is really interesting. I've kind of thought of optimization before, but it is really interesting to think of it as Constant, Linear, and Quadratic. I'm interested in working in games, and thinking of ways Linear algorithms could be used is really interesting (like storing randomly generated enemies in a list and pulling them out one at a time etc).
